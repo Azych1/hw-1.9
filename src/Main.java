d4a6bc544be1378870e1844d5b41a0509c3068fa
Azych1 <nagimov123@gmail.com>
2023-05-30 00:29:33 +0500

    ДЗ сделано 2

--- src/Main.java
+++ src/Main.java
@@ -7,23 +7,23 @@ public class Main {
 
     }
 
-    public static int[] generateRandomArray(){
+    public static int[] generateRandomArray() {
         java.util.Random random = new java.util.Random();
         int[] arr = new int[30];
-        for (int i = 0; i < arr.length; i++){
+        for (int i = 0; i < arr.length; i++) {
             arr[i] = random.nextInt(100_000) + 100_000;
         }
         return arr;
     }
 
-    public static void task1 () {
+    public static void task1() {
         System.out.println("Задача 1");
         int[] budget = generateRandomArray();
         int sum = 0;
 
         for (int i = 0; i < budget.length; i++) {
             sum += budget[i];
-            System.out.println("В день "+(i+1)+" было потрачено "+sum);
+            System.out.println("В день " + (i + 1) + " было потрачено " + sum);
         }
         System.out.println();
         System.out.println("Задача 2");
@@ -31,16 +31,16 @@ public class Main {
         int max = budget[0];
 
         for (int i = 0; i < budget.length; i++) {
-            if(budget[i] < min){
+            if (budget[i] < min) {
                 min = budget[i];
             }
-            if(budget[i] > max){
+            if (budget[i] > max) {
                 max = budget[i];
             }
         }
 
-        System.out.println("Минимальная сумма трат за день составила "+min+" рублей. " +
-                "Минимальная сумма трат за день составила "+max+" рублей.");
+        System.out.println("Минимальная сумма трат за день составила " + min + " рублей. " +
+                "Минимальная сумма трат за день составила " + max + " рублей.");
         System.out.println();
         System.out.println("Задача 3");
 
@@ -51,15 +51,15 @@ public class Main {
             days++;
         }
 
-        System.out.printf("Потратили: %.0f"+". Среднее значение: %.2f", summ, summ/days);
+        System.out.printf("Потратили: %.0f" + ". Среднее значение: %.2f", summ, summ / days);
         System.out.println();
         System.out.println();
 
     }
 
-    public static void task4 () {
+    public static void task4() {
         System.out.println("Задача 4");
-        char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
 
         for (int i = reverseFullName.length - 1; i >= 0; i--) {
             System.out.print(reverseFullName[i]);
