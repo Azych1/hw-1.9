c79de6c1b4f861bd26b83500c20f166c6fd2729a
Azych1 <nagimov123@gmail.com>
2023-05-30 00:28:55 +0500

    ДЗ сделано

--- src/Main.java
+++ src/Main.java
@@ -1,55 +1,70 @@
 public class Main {
     public static void main(String[] args) {
         task1();
-        task2();
-        task3();
-        task4();
-        task5();
-        task6();
-        task7();
-        task8();
 
+        task4();
+
+
+    }
+
+    public static int[] generateRandomArray(){
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++){
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
     }
 
     public static void task1 () {
         System.out.println("Задача 1");
+        int[] budget = generateRandomArray();
+        int sum = 0;
 
-
-    }
-
-    public static void task2 () {
+        for (int i = 0; i < budget.length; i++) {
+            sum += budget[i];
+            System.out.println("В день "+(i+1)+" было потрачено "+sum);
+        }
+        System.out.println();
         System.out.println("Задача 2");
+        int min = budget[0];
+        int max = budget[0];
 
-    }
+        for (int i = 0; i < budget.length; i++) {
+            if(budget[i] < min){
+                min = budget[i];
+            }
+            if(budget[i] > max){
+                max = budget[i];
+            }
+        }
 
-    public static void task3 () {
+        System.out.println("Минимальная сумма трат за день составила "+min+" рублей. " +
+                "Минимальная сумма трат за день составила "+max+" рублей.");
+        System.out.println();
         System.out.println("Задача 3");
 
+        double summ = 0;
+        double days = 0;
+        for (int i = 0; i < budget.length; i++) {
+            summ += budget[i];
+            days++;
+        }
+
+        System.out.printf("Потратили: %.0f"+". Среднее значение: %.2f", summ, summ/days);
+        System.out.println();
+        System.out.println();
 
     }
 
     public static void task4 () {
         System.out.println("Задача 4");
+        char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
 
-    }
-
-    public static void task5 () {
-        System.out.println("Задача 5");
-
-    }
-
-    public static void task6 () {
-        System.out.println("Задача 6");
-
-    }
-
-    public static void task7 () {
-        System.out.println("Задача 7");
-
-    }
-    public static void task8 () {
-        System.out.println("Задача 8");
-
+        for (int i = reverseFullName.length - 1; i >= 0; i--) {
+            System.out.print(reverseFullName[i]);
+        }
+        System.out.println();
     }
 
 
